aafa64a3e6a974ddd285d16d71c88efaeb3028ad
Erik <bako.erik.dominik@gmail.com>
2024-06-01 14:08:23 +0200

    t5 changed to facebook's bart for summarizing bigger texts

--- python/PythonSummarize.py
+++ python/PythonSummarize.py
@@ -3,17 +3,17 @@ from transformers import pipeline
 
 app = Flask(__name__)
 #initializing pipeline with the transformers library
-summarizer = pipeline("summarization", model="t5-base")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 #defining route
 @app.route('/summarize', methods=['POST'])
 def summarize():
-    #ectract json data from the post request
+    #extract json data from the post request
     data = request.get_json()
     #extracting the text field
     text = data['text']  
     #generate the summary with the pipeline
-    summary = summarizer(text, max_length=150, min_length=20, do_sample=False)
+    summary = summarizer(text, max_length=500, min_length=20, do_sample=True)
     #extract the text from the summary
     summarized_text = summary[0]['summary_text']
     #return the text in json format
